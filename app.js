336ba99f60d5646c2c3a10afdd95a40d0c6f18af
vivek <vivek.sarabu@gmail.com>
2016-05-20 14:45:13 -0400

    updated comments
    updated comments

--- app.js
+++ app.js
@@ -16,9 +16,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 app.use(express.static('public'));
-
 	//children.findAll(); finding from in memory database.
-
 app.get('/employees', function  (req, res) {
   res.json(jsonfile.readFileSync(file));
 });
@@ -32,23 +30,11 @@ app.post('/employeeModifications', urlencodedParser, function  (req, res) {
 	 //       email:req.body.email,
 	 //       role:req.body.role
 	 //   });
-	// console.log(req.body.firstName);
-
-	/*response = {
-       first_name:req.body.firstName,
-       last_name:req.body.lastName,
-       last_name:req.body.email
-
-   };*/
-   //console.log(response);
 
    jsonfile.writeFileSync(file, req.body)
    res.send("success");
 });
 
-
 var server = app.listen(3000, function () {
-
   console.log("Application is running on http://localhost:3000/");
-
 })
